6fc234936e07fb3c02ba14e484892ff713917f16
angelmizutani <angelisayuri@yahoo.com.br>
2022-06-13 20:19:40 -0300

    atividade13jun

--- Lovebox/usuarios/views.py
+++ Lovebox/usuarios/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import TemplateView
+from Lovebox.cadastros.models import Medicamento
+from braces.views import GroupRequiredMixin
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
+class MedicamentoList(GroupRequiredMixin, LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
+    group_required = u"Administrador"
+    model = Medicamento
+    template_name = 'cadastros/listar-medicamentos.html'
