17c023328332a697d591de1f6aee4c699f5d61d3
brenda-stefany-americo <brendamerico1@gmail.com>
2022-09-05 21:36:36 -0300

    aula05set22

--- Lovebox/usuarios/urls.py
+++ Lovebox/usuarios/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views
 
 urlpatterns = [
     path('entrar/', views.LoginView.as_view(
-        template_name='usuarios/login.html'
+        template_name='usuarios/login.html',
+        extra_context={'titulo' : 'Autenticação'}
     ), name='login'),
     path('sair/', views.LogoutView.as_view(), name='logout'),
+    path('alterar-minha-senha/', views.PasswordChangeView.as_view(
+        template_name='usuarios/login.html',
+        extra_context={'titulo' : 'Alterar senha atual'},
+        success_url=reverse_lazy('index')
+    ), name="alterar-senha"),
 ]
